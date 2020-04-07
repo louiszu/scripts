830ff22c4e3ee26cb840422ee1f830ddac8ca66c
louiszu <davidany@yeah.net>
2020-04-07 10:33:00 +0800

    Update docker-image.sh

--- docker-image.sh
+++ docker-image.sh
@@ -11,8 +11,9 @@ DOCKER_COMPOSE_FILE=$3
 eval $(aws ecr get-login --no-include-email)
 
 #step2: 更新镜像tag
-tags=`cat $DOCKER_COMPOSE_FILE | grep $1 | awk -F - '{print $NF}'`
-sed -i -e "s#$tags#$2#g" $DOCKER_COMPOSE_FILE
+tags=`cat $DOCKER_COMPOSE_FILE | grep image| grep $1 | awk -F - '{print $NF}'`
+sed -i "s#$1-$tags#$1-$2#g" $DOCKER_COMPOSE_FILE
+
 
 #step3: 启动
 docker-compose -f $DOCKER_COMPOSE_FILE up -d
